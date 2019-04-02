56650b95c829398fca471ea6cf2b1f86653f3caa
Ilia Volkov <ilya.j0b@yandex.ru>
2019-04-02 18:57:28 +0300

    add array as a prop

--- src/App.js
+++ src/App.js
@@ -5,12 +5,17 @@ import TodoList from "./components/todo-list/todo-list";
 import TodoListItem from "./components/todo-list-item/todo-list-item";
 
 export default class App extends Component {
+  toDos = [
+    { id: 1, label: "drink a coffe" },
+    { id: 2, label: "to read a book" },
+    { id: 2, label: "to create something" }
+  ];
   render() {
     return (
       <div>
         <Header />
         <SearchPanel />
-        <TodoList />
+        <TodoList toDos={this.toDos} />
       </div>
     );
   }
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -1,17 +1,17 @@
 import React from "react";
 import TodoListItem from "./../todo-list-item/todo-list-item";
 
-const TodoList = () => {
+const TodoList = ({ toDos }) => {
+  const elements = toDos.map(item => {
+    return (
+      <li>
+        <TodoListItem key={item.id} label={item.label} />
+      </li>
+    );
+  });
   return (
     <div>
-      <ul>
-        <li>
-          <TodoListItem label="To read a book" />
-        </li>
-        <li>
-          <TodoListItem label="To go shoping" />
-        </li>
-      </ul>
+      <ul>{elements}</ul>
     </div>
   );
 };
